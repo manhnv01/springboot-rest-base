0ea2b3a5d2734f4be33df502fdc2d33408b01c43
manhnv01 <manhnv291201@gmail.com>
2025-07-10 13:20:34 +0700

    code base

--- src/main/java/io/github/manhnv01/springbootrestbase/exceptions/ExceptionResponse.java
+++ src/main/java/io/github/manhnv01/springbootrestbase/exceptions/ExceptionResponse.java
@@ -11,36 +11,40 @@ import java.util.Date;
 @NoArgsConstructor
 @AllArgsConstructor
 public class ExceptionResponse {
-  private Integer status;
-  private Date timestamp;
-  private String message;
-  private String messageCode;
-  private String description;
-  private String path;
+    private Integer status;
+    private Date timestamp;
+    private String message;
+    private String messageCode;
+    private String description;
+    private String path;
 
-  public ExceptionResponse(
-      HttpStatus status, Date timestamp, String message, String description, String path) {
-    super();
-    this.status = status.value();
-    this.timestamp = timestamp;
-    this.message = message;
-    this.description = description;
-    this.path = path;
-  }
+    public ExceptionResponse(
+            HttpStatus status,
+            Date timestamp,
+            String message,
+            String description,
+            String path) {
+        super();
+        this.status = status.value();
+        this.timestamp = timestamp;
+        this.message = message;
+        this.description = description;
+        this.path = path;
+    }
 
-  public ExceptionResponse(
-      HttpStatus status,
-      Date timestamp,
-      String message,
-      String messageCode,
-      String description,
-      String path) {
-    super();
-    this.status = status.value();
-    this.timestamp = timestamp;
-    this.message = message;
-    this.messageCode = messageCode;
-    this.description = description;
-    this.path = path;
-  }
+    public ExceptionResponse(
+            HttpStatus status,
+            Date timestamp,
+            String message,
+            String messageCode,
+            String description,
+            String path) {
+        super();
+        this.status = status.value();
+        this.timestamp = timestamp;
+        this.message = message;
+        this.messageCode = messageCode;
+        this.description = description;
+        this.path = path;
+    }
 }
