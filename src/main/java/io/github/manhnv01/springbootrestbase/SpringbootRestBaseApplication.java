5540dca772a47c3dfbc0b531e139fc33810ab868
manhnv01 <manhnv291201@gmail.com>
2025-07-10 10:21:31 +0700

    Initial commit

---
+++ src/main/java/io/github/manhnv01/springbootrestbase/SpringbootRestBaseApplication.java
@@ -0,0 +1,13 @@
+package io.github.manhnv01.springbootrestbase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootRestBaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootRestBaseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/github/manhnv01/springbootrestbase/SpringbootRestBaseApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.manhnv01.springbootrestbase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootRestBaseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
